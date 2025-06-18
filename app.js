87ee4a445164960c59abfc73220c6cc547c6c6d3
Rohith Aryan <aryanadike100@gmail.com>
2025-06-18 12:56:45 +0530

    Added Project Files

---
+++ app.js
@@ -0,0 +1,85 @@
+if(process.env.NODE_ENV!="production"){
+    require('dotenv').config();
+}
+
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate"); 
+const ExpressError = require("./utils/ExpressError.js"); 
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js")
+
+const dburl = process.env.DB_URL;
+
+main()
+    .then((res)=>{
+        console.log("connected to db");
+    }).catch((err)=>console.log(err));
+
+async function main() {
+  await mongoose.connect(dburl);
+}
+
+
+
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
+
+
+// session
+const sessionOptions = {
+    secret : process.env.SECRET,
+    resave : false,
+    saveUninitialized : true,
+    cookie : {
+        expires : Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge : 7 * 24 * 60 * 60 * 1000,
+        httpOnly : true,
+    },
+};
+
+app.use(session(sessionOptions));
+app.use(flash())
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next)=>{
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    res.locals.currUser = req.user;
+    next();
+});
+
+
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter); 
+
+
+app.use((err, req, res, next) => {
+    let {statusCode=500, message="something went wrong"} = err;
+    res.status(statusCode).render("listings/error.ejs", {message});
+}); 
+
+
+app.listen(8080, ()=>{
+    console.log("Server is listening to port 8080");
+});
---
+++ init/index.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+  await Listing.deleteMany({});
+  initData.data = initData.data.map(obj => ({
+  ...obj,
+  owner: "684bf66d9c3b5be2275baa24",
+  geometry: obj.geometry ?? {
+    type: "Point",
+    coordinates: [77.1025, 28.7041] // default Delhi coordinates
+  }
+}));
+
+  await Listing.insertMany(initData.data);
+  console.log("data was initialized");
+};
+
+initDB();
